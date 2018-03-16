07b3f5f99562859ef930817fbb99c933a6b39e42
anchitj2ee <anchitj2ee@gmail.com>
2018-03-17 04:08:16 +0530

    Initial commit

---
+++ app/src/main/java/com/onistech/user/Dashboard.java
@@ -0,0 +1,20 @@
+package com.onistech.user;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.onistech.user.moranmandi.R;
+
+/**
+ * Created by user on 3/16/2018.
+ */
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
